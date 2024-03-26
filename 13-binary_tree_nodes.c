54124bc4bbca2d9e4d12937141faf3fdc5f7ab10
Collins Kimani <mwas9446.ck@gmail.com>
2024-03-26 21:57:45 +0300

    handles betty coding style error

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -16,7 +16,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	left = binary_tree_nodes(tree->left);
 	right = binary_tree_nodes(tree->right);
-	
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
